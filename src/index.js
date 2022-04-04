aad932446efa3c7f9330bd3ffa19e40dc41765dd
rgomeztinoco <gricardo2213@gmail.com>
2022-04-03 20:10:34 -0500

    📦 curso webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import Template from './templates/Template.js';
-console.log('hola');
+import Template from "@templates/Template.js";
+import "@styles/main.css";
+import "@styles/vars.styl";
 
 (async function App() {
-  const main = null || document.getElementById('main');
+  const main = null || document.getElementById("main");
   main.innerHTML = await Template();
 })();
